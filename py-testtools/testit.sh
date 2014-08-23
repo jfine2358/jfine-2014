ed837f34a62e43e32bfc9493a737d6e27ac4239f
Jonathan Fine <jfine@pytex.org>
2014-08-23 19:02:11 +0100

    Have selftest.py evaluate suitable expressions - no need now to test simpletest.py.

--- py-testtools/testit.sh
+++ py-testtools/testit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python2 testtools/simpletest.py
-python3 testtools/simpletest.py
+# python2 testtools/simpletest.py
+# python3 testtools/simpletest.py
 
 python2 testtools/selftest.py
 python3 testtools/selftest.py
--- py-testtools/testtools/selftest.py
+++ py-testtools/testtools/selftest.py
@@ -13,8 +13,30 @@ with open(filename) as f:
 if 0:
     print(ast.dump(tree))
 
+
+def add(x, y):
+    return x + y
+
+
 if 1:
+    # Pick out and evaluate all suitable expressions.
     for line in tree.body:
-        # Each line has a name, which determines its type.
-        name = type(line.value).__name__
-        print(name)
+        value = line.value
+
+        if type(value) is ast.Compare:
+            ops = value.ops
+            # Make a test for this.
+            if [op for op in ops if type(op) is not ast.Eq]:
+                raise ValueError
+
+            aaa = [value.left] + value.comparators
+
+            bbb = [
+                compile(ast.Expression(aa), '', 'eval')
+                for aa in aaa
+                ]
+
+            print(tuple(
+                    eval(bb, dict(add=add))
+                    for bb in bbb
+                    ))
